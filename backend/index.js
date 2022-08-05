2b16f5c91da4c9d980c58dae4ee936fbc3beb6f8
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-08-05 10:54:53 +0200

    conectend details added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
 
 const app = express();
 const PORT = 3000;
+
+// mongo db conection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/soccerDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// body parser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => 
 res.send(`Soccer app is runnig  ${PORT}`)
